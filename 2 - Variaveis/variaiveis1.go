3b563a4f024c67712d468d4450d602c8f6b1ad0b
Gabriel <gabriel@MacBook.local>
2022-11-07 15:26:32 -0800

    2- tipos de variaveis

---
+++ 2 - Variaveis/variaiveis1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string = "Gabriel"
+	var idade int = 35
+	var versao float64 = 1.18
+	fmt.Println("Olá, sr.", nome, "sua idade é ", idade)
+	fmt.Println("A nossa versão do Go é :", versao)
+}
