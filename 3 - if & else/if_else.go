bdfbe455d01135f020be9df72181222dfe35e949
Gabriel <gabriel@MacBook.local>
2022-11-07 17:39:15 -0800

    If e Else

---
+++ 3 - if & else/if_else.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nome := "Gabriel"
+	versao := 1.1
+	fmt.Println("Olá Sr.", nome)
+	fmt.Println("A versão do programa de monitoramento é ", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	//fmt.Println("A opção escolhida foi", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do Programa...")
+	} else {
+		fmt.Println("Comando inválido.")
+	}
+
+}
