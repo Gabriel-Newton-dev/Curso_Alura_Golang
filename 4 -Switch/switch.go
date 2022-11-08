f45bacc40e1e0b5309892fee855674d41887555b
Gabriel <gabriel@MacBook.local>
2022-11-07 18:17:43 -0800

    Switch Alura

---
+++ 4 -Switch/switch.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nome := "Gabriel"
+	versao := 1.1
+	fmt.Println("Olá Sr.", nome)
+	fmt.Println("A versão do programa de monitoramento é ", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	//fmt.Println("A opção escolhida foi", comando)
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+		break
+	case 2:
+		fmt.Println("Exibindo logs...")
+		break
+	case 0:
+		fmt.Println("Saindo do programa...")
+		break
+	default:
+		fmt.Println("Comando Inválido")
+	}
+
+}
