7429c101ce52afc9111db1f8ae558502cd0911d3
Gabriel <gabriel@MacBook.local>
2022-11-23 20:35:16 -0800

    Funcao Sacar e funcao depositar

--- 31 - bank/bank.go
+++ 31 - bank/bank.go
@@ -9,6 +9,17 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) {
+	c.saldo -= valorDoSaque
+	fmt.Printf("O valor do saque foi de R$ %v Reais, o saldo atualizado é de R$ %v Reais\n", valorDoSaque, c.saldo)
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) {
+	c.saldo += valorDoDeposito
+	fmt.Printf("O valor do Deposito foi de R$ %v Reais, seu saldo atualizado é de R$ %v Reais", valorDoDeposito, c.saldo)
+
+}
+
 func main() {
 
 	contaDoGuilherme := ContaCorrente{"Guilherme Dias", 1612, 256969, 187.60}
@@ -17,4 +28,8 @@ func main() {
 
 	fmt.Println(contaDoGuilherme)
 	fmt.Println(contaDaBruna)
+	contaDoGuilherme.Sacar(10)
+	fmt.Println(contaDoGuilherme)
+	contaDaBruna.Sacar(10)
+	contaDoGuilherme.Depositar(1000)
 }
