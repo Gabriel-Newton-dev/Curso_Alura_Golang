6eb047ee19eb4b29ff943054e1987fe1776c076b
Gabriel <gabriel@MacBook.local>
2022-11-25 00:56:56 -0800

    Funcao transferencia

--- 31 - bank/bank.go
+++ 31 - bank/bank.go
@@ -10,26 +10,47 @@ type ContaCorrente struct {
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) {
-	c.saldo -= valorDoSaque
-	fmt.Printf("O valor do saque foi de R$ %v Reais, o saldo atualizado é de R$ %v Reais\n", valorDoSaque, c.saldo)
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		fmt.Printf("O valor do saque foi de R$ %v Reais, o saldo atualizado é de R$ %v Reais\n", valorDoSaque, c.saldo)
+	} else {
+		fmt.Println("Saldo Insuficiente")
+	}
+
 }
 
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) {
-	c.saldo += valorDoDeposito
-	fmt.Printf("O valor do Deposito foi de R$ %v Reais, seu saldo atualizado é de R$ %v Reais", valorDoDeposito, c.saldo)
+	podeDepositar := valorDoDeposito >= 0
+	if podeDepositar {
+		c.saldo += valorDoDeposito
+		fmt.Printf("O valor do Deposito foi de R$ %v Reais, seu saldo atualizado é de R$ %v Reais\n", valorDoDeposito, c.saldo)
+	} else {
+		fmt.Println("Não é possível depositar valor negativo")
+	}
+}
 
+func (c *ContaCorrente) Transferencia(valorDaTransferencia float64, contaDestino *ContaCorrente) {
+	podeTransferir := valorDaTransferencia < c.saldo && valorDaTransferencia > 0
+	if podeTransferir {
+		c.saldo -= valorDaTransferencia
+		contaDestino.saldo += valorDaTransferencia
+		fmt.Printf("Valor de R$ %v Reais, foi transferido com sucesso, para conta do %v.\n", valorDaTransferencia, contaDestino.titular)
+	} else {
+		fmt.Println("Transferência não realizada")
+	}
 }
 
 func main() {
 
 	contaDoGuilherme := ContaCorrente{"Guilherme Dias", 1612, 256969, 187.60}
 
-	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+	contaDaJurema := ContaCorrente{"Jurema", 222, 111222, 500}
 
-	fmt.Println(contaDoGuilherme)
-	fmt.Println(contaDaBruna)
-	contaDoGuilherme.Sacar(10)
-	fmt.Println(contaDoGuilherme)
-	contaDaBruna.Sacar(10)
-	contaDoGuilherme.Depositar(1000)
+	contaDoGuilherme.Sacar(1000)
+	contaDaJurema.Sacar(1000)
+	contaDoGuilherme.Depositar(13)
+	contaDaJurema.Transferencia(200, &contaDoGuilherme)
+	fmt.Println("R$", contaDaJurema.saldo, "Reais")
+	fmt.Println("R$", contaDoGuilherme.saldo, "Reais")
 }
